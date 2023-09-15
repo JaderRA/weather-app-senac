d033881943d5171eafed9e04b91f926de8a3027d
Jader Raniere Albrez <jader.albrez@parceiro.gruposaltaedu.com>
2023-09-14 21:10:55 -0300

    feat: added new validations to disable or enable home buttons

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,33 @@
 window.addEventListener("load", function () {
-  cidades = JSON.parse(localStorage.cidades);
-
-  for (let i = 0; i < 4; i++) {
-    if (cidades[i]) {
-      this.document.getElementById(`Lavoura ${i + 1}`).innerHTML = cidades[i];
-    } else {
-      this.document.getElementById(`Lavoura ${i + 1}`).innerHTML = `Lavoura ${
-        i + 1
-      }`;
+  if (localStorage.cidades) {
+    cidades = JSON.parse(localStorage.cidades);
+    for (let i = 0; i < 4; i++) {
+      button = this.document.getElementById(`Lavoura ${i + 1}`);
+      if (cidades[i]) {
+        button.innerHTML = cidades[i];
+        button.disabled = false;
+        button.classList.remove("disabled-button");
+      } else {
+        button.innerHTML = `Lavoura ${i + 1}`;
+        button.disabled = true;
+        button.classList.add("disabled-button");
+      }
     }
+  } else {
+    disableButtons();
   }
 });
+
+function disableButtons() {
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((element) => {
+    if (element.textContent.includes("Lavoura")) {
+      element.disabled = true;
+      element.classList.add("disabled-button");
+    }
+  });
+}
 
 function salvarIdBotao(idBotao) {
   localStorage.setItem("idBotao", JSON.stringify(idBotao));
